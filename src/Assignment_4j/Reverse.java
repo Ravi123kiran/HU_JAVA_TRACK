80fc3c48463d218544f8f8ee9feb5ac3d9426667
ravi123kiran <ravikumaa@deloitte.com>
2022-03-17 17:24:06 +0530

    assignment4

---
+++ src/Assignment_4j/Reverse.java
@@ -0,0 +1,20 @@
+package Assignment_4j;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Reverse {
+    public static void main(String[] args) {
+        ArrayList<String>company = new ArrayList<String>();
+        company.add("hashediin");
+        company.add("by");
+        company.add("deloitte");
+        Collections.reverse(company);
+        System.out.println(company);
+
+
+        }
+    }
+
+
