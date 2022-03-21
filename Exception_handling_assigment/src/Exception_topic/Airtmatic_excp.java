42fa2d82c63bf63f37d5e8654e2451c6e5cc9bac
ravi123kiran <ravikumaa@deloitte.com>
2022-03-21 10:19:40 +0530

    Airthematic exception added

---
+++ Exception_handling_assigment/src/Exception_topic/Airtmatic_excp.java
@@ -0,0 +1,20 @@
+package Exception_topic;
+
+public class Airtmatic_excp {
+    public static void main(String[] args) {
+        System.out.println("****main starts****");
+
+        try {
+
+            int i = 1 / 10;
+        }
+        finally {
+            System.out.println("Thank you");
+
+        }
+        System.out.println("****main ends****");
+        }
+        {
+
+    }
+}
